648588a7c4e5467196c50928c2e20427f64af16b
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-10-17 10:11:42 +0100

    fixed collapse issue for static version

--- src/components/template2/SideNavBar.jsx
+++ src/components/template2/SideNavBar.jsx
@@ -13,104 +13,121 @@ import Collapse from 'public/Images/template2/Subtract.svg'
 import Gift from "public/Images/template2/Gift.svg";
 import Gift1 from "public/Images/template2/Gift1.svg";
 import Gift2 from "public/Images/template2/Gift2.svg";
+import { useState } from 'react' ;
 
-import { useState } from 'react'
 
-
-const SideNavBar = () => {
-  const [active, setActive] = useState(true)
-    return (
-      <aside className="flex flex-col items-center justify-between h-[100vh] gap-5 px-6 py-5 overflow-y-scroll bg-white w-80">
-        {/* a Wrapper div  */}
-        <div className="mt-2">
-          {/* Profile pic and preview */}
-          <div className="flex items-center px-5 py-2 border-2 justify-evenly rounded-3xl">
-            <Image src={Pic} alt="Profile Picture" />
-            <div className="px-3">
-              <h1 className="text-lg font-bold text-primary">Kate Russell</h1>
-              <p className="text-sm opacity-50 text-primary">Project Manager</p>
-            </div>
-            <Image src={Arrow} />
+const SideNavBar = ({
+  isLogedIn,
+  logo,
+  links,
+  generaleStyles,
+  layoutStyles,
+  buttonsActions,
+}) => {
+  const [Open, setOpen] = useState(true);
+  return (
+    <aside
+      className={`flex flex-col items-center justify-between h-[100vh] gap-5  overflow-y-scroll overflow-x-hidden bg-white duration-300 ${
+        Open ? "w-80 px-6 py-5" : "w-20 py-0 p-0"
+      }`}
+    >
+      {/* a Wrapper div  */}
+      <div className="w-full mt-2">
+        {/* Profile pic and preview */}
+        <div
+          className={`flex items-center justify-evenly rounded-3xl w-full ${
+            Open ? "px-3 border-2 py-2 " : "p-0 mt-6 border-0"
+          }`}
+        >
+          <Image src={Pic} alt="Profile Picture" className="w-12" />
+          <div className={`px-3 duration-300 ${!Open && "hidden px-0"}`}>
+            <h1 className="text-lg font-bold text-primary">Kate Russell</h1>
+            <p className="text-sm opacity-50 text-primary">Project Manager</p>
           </div>
-          {/* Links Overview */}
-          <div className="flex flex-col mt-4">
-            <Link href={"/"}>
-              <div className="flex items-center gap-4 p-4">
-                <Image alt="Overview" src={Overview} />
-                <h1 className="text-lg font-medium text-primary">Overview</h1>
-              </div>
-            </Link>
-            {/* Shop Item */}
-            <Link href={"/"}>
-              <div className="flex items-center gap-4 p-4">
-                <Image alt="Shop" src={Shop} />
-                <h1 className="text-lg font-medium text-primary">Shop</h1>
-              </div>
-            </Link>
-            {/* Released Item */}
-            <Link href={"/"}>
-              <div className="flex items-center gap-4 p-4">
-                <Image alt="View List" src={List} />
-                <h1 className="text-lg font-medium text-primary">Released</h1>
-              </div>
-            </Link>
-            {/* Comments Item */}
-            <Link href={"/"}>
-              <div className="flex items-center gap-4 p-4">
-                <Image alt="View List" src={Chat} />
-                <h1 className="text-lg font-medium text-primary">Comments</h1>
-              </div>
-            </Link>
-            <div className="flex flex-col justify-center">
-              {/* Settings Item */}
-              <Link href={"/"}>
-                <div className="flex items-center gap-4 p-4 border-1">
-                  <Image alt="circle and line" src={Settings} />
-                  <h1 className="text-lg font-medium text-primary">Settings</h1>
-                </div>
-              </Link>
-              <div className="flex flex-col justify-center px-5">
-                {/*First Sub Settings Item */}
-                <div className="flex items-center gap-4 border-1">
-                  <Image alt="circle and line" src={Gift} />
-                  <h1 className="text-base font-medium text-gray-500">
-                    Edit Profile
-                  </h1>
-                </div>
-                {/*Middle Sub Settings Item */}
-                <div className="flex items-center gap-4 border-1">
-                  <Image alt="circle and line" src={Gift1} />
-                  <h1 className="text-base font-medium text-gray-500">
-                    Language
-                  </h1>
-                </div>
-                {/*Middle Sub Settings Item */}
-                <div className="flex items-center gap-4 border-1">
-                  <Image alt="circle" src={Gift1} />
-                  <h1 className="text-base font-medium text-[#317BFF]">
-                    Payment
-                  </h1>
-                </div>
-                <div className="flex items-center gap-4 border-1">
-                  <Image alt="circle and line" src={Gift2} />
-                  <h1 className="text-base font-medium text-gray-500">
-                    Payment
-                  </h1>
-                </div>
-              </div>
-            </div>
-            {/* Calendar Item */}
-            <Link href={"/"}>
-              <div className="flex items-center gap-4 p-4">
-                <Image alt="View List" src={Calendar} />
-                <h1 className="text-lg font-medium text-primary">Schedule</h1>
-              </div>
-            </Link>
-          </div>
-        </div>
-        {/* Wrapper Div */}
-        <div>
           <Link href={"/"}>
+            <Image
+              src={Arrow}
+              className={`duration-300 ${!Open && "hidden"}`}
+            />
+          </Link>
+        </div>
+        {/* Links Overview */}
+        <div className={`flex flex-col ${Open && "mt-4" }`}>
+          <Link href={"/"}>
+            <div className="flex items-center gap-4 p-4">
+              <Image alt="Overview" src={Overview} />
+              <h1 className={`text-lg font-medium text-primary ${!Open && "hidden"}`}>Overview</h1>
+            </div>
+          </Link>
+          {/* Shop Item */}
+          <Link href={"/"}>
+            <div className="flex items-center gap-4 p-4">
+              <Image alt="Shop" src={Shop} />
+              <h1 className="text-lg font-medium text-primary">Shop</h1>
+            </div>
+          </Link>
+          {/* Released Item */}
+          <Link href={"/"}>
+            <div className="flex items-center gap-4 p-4">
+              <Image alt="View List" src={List} />
+              <h1 className="text-lg font-medium text-primary">Released</h1>
+            </div>
+          </Link>
+          {/* Comments Item */}
+          <Link href={"/"}>
+            <div className="flex items-center gap-4 p-4">
+              <Image alt="View List" src={Chat} />
+              <h1 className="text-lg font-medium text-primary">Comments</h1>
+            </div>
+          </Link>
+          <div className="flex flex-col justify-center">
+            {/* Settings Item */}
+            <Link href={"/"}>
+              <div className="flex items-center gap-4 p-4 border-1">
+                <Image alt="circle and line" src={Settings} />
+                <h1 className="text-lg font-medium text-primary">Settings</h1>
+              </div>
+            </Link>
+            <div className="flex flex-col justify-center px-5">
+              {/*First Sub Settings Item */}
+              <div className="flex items-center gap-4 border-1">
+                <Image alt="circle and line" src={Gift} />
+                <h1 className="text-base font-medium text-gray-500">
+                  Edit Profile
+                </h1>
+              </div>
+              {/*Middle Sub Settings Item */}
+              <div className="flex items-center gap-4 border-1">
+                <Image alt="circle and line" src={Gift1} />
+                <h1 className="text-base font-medium text-gray-500">
+                  Language
+                </h1>
+              </div>
+              {/*Middle Sub Settings Item */}
+              <div className="flex items-center gap-4 border-1">
+                <Image alt="circle" src={Gift1} />
+                <h1 className="text-base font-medium text-[#317BFF]">
+                  Payment
+                </h1>
+              </div>
+              <div className="flex items-center gap-4 border-1">
+                <Image alt="circle and line" src={Gift2} />
+                <h1 className="text-base font-medium text-gray-500">Payment</h1>
+              </div>
+            </div>
+          </div>
+          {/* Calendar Item */}
+          <Link href={"/"}>
+            <div className="flex items-center gap-4 p-4">
+              <Image alt="View List" src={Calendar} />
+              <h1 className="text-lg font-medium text-primary">Schedule</h1>
+            </div>
+          </Link>
+        </div>
+      </div>
+      {/* Wrapper Div */}
+      <div>
+        {/* <Link href={"/"}>
             <div className="flex items-center justify-center gap-4 px-6 py-3 border-2 border-gray-300 rounded-2xl">
               <Image alt="Support" src={Support} />
               <div>
@@ -119,16 +136,27 @@ const SideNavBar = () => {
               </div>
               <Image src={Arrow} className="-rotate-90" />
             </div>
-          </Link>
-          <div className="flex items-center gap-4 p-4">
-            <Image alt="Collapse" src={Collapse} />
-            <h1 className="text-lg font-medium text-gray-400">
-              Collapse items
-            </h1>
-          </div>
+          </Link> */}
+        <div
+          className={`flex items-center gap-4 cursor-pointer ${
+            Open ? "p-4" : "p-0"
+          }`}
+          onClick={() => {
+            setOpen(!Open);
+          }}
+        >
+          <Image alt="Collapse" src={Collapse} />
+          <h1
+            className={`text-lg font-medium text-gray-400 ${
+              !Open && "scale-0"
+            }`}
+          >
+            Collapse items
+          </h1>
         </div>
-      </aside>
-    );
-}
+      </div>
+    </aside>
+  );
+};
  
 export default SideNavBar;

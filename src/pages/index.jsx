7375f8ebaa828570b9f8d0e8e598d5c87381d158
imadeddineF <imadeddinefillalipro@gmail.com>
2023-10-18 21:00:24 +0100

    navbar 3 first version

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,44 @@
-import Sidenavbar3 from "@/components/template3/SideNabBar"
-import SideBarWithSearch from "@/components/SidebarWithSearch/SideBarWithSearch"
-const Home = () => {
-  return (
-    <div className="w-full h-full bg-white" >
-      <SideBarWithSearch/>
-    </div>
-  )
-}
+import Sidenavbar3 from "@/components/template3/SideNabBar";
+import SideBarWithSearch from "@/components/SidebarWithSearch/SideBarWithSearch";
+import NavbarThree from "@/components/navbar3/navbarThree";
+import logo from "../../public/Images/template1/Navbar/Logo.svg";
 
-export default Home
+const Home = () => {
+  const customData = [
+    {
+      title: "customOne",
+      subTitles: ["One", "One", "One"],
+      subTitlesLinks: ["/", "/", "/"],
+    },
+    {
+      title: "customTwo",
+      subTitles: ["Two", "Two", "Two"],
+      subTitlesLinks: ["/", "/", "/"],
+    },
+    {
+      title: "customThree",
+      subTitles: ["Three", "Three", "Three"],
+      subTitlesLinks: ["/", "/", "/"],
+    },
+  ];
+  return (
+    <div className="w-full h-full bg-white">
+      {/* <SideBarWithSearch /> */}
+      <NavbarThree
+        logo={logo}
+        logoWidth="180"
+        logoHeight=""
+        data={customData}
+        btnOne="btn one"
+        linkBtnOne="/"
+        btnTwo="btn two"
+        linkBtnTwo="/"
+        navBgColor="#DDDDDD"
+        navHeight="80px"
+        mobileSideBarBg="#BBBBBB"
+      />
+    </div>
+  );
+};
+
+export default Home;

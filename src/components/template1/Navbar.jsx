b4afa7ccfa37c8de09785f0fab7cc42504854edd
Ryaaad <devil.fine116@gmail.com>
2023-10-18 13:57:43 +0100

    costumized navbar1 without responsivness

--- src/components/template1/Navbar.jsx
+++ src/components/template1/Navbar.jsx
@@ -1,23 +1,42 @@
-import logo from "public/Images/template1/Navbar/Logo.svg";
 import Image from "next/image";
 import Link from "next/link";
-import profilpic from "public/Images/template1/Navbar/profilpic.svg"
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { links , loggedIn,logo ,layout,textColor,textSize,menuType,logoProfil} = props;
   return (
-    <div className="flex items-center justify-between pr-3 w-full bg-[#fff] h-24 ">
+    <div className={`flex items-center justify-between pr-3 w-full bg-[#fff] h-24 ${ layout=="Fixed" && "fixed"  } top-0 `}>
       <div className="flex items-center gap-10">
         <Image alt="logo" src={logo} />
-        <div className="flex items-center gap-16 text-[#2D2E2E] ">
-          <Link href={"/"}> Catalog </Link>
-          <Link href={"/"}>Delivery</Link>
-          <Link href={"/"}>Payment</Link>
-          <Link href={"/"}>Contact</Link>
+        <div style={{color:`${textColor}`, fontSize:`${textSize}`}} className={`flex items-center gap-16 `}>
+          {links.map((link, index) => {
+            return (
+              <Link key={index} href={`${link.route} `}  >
+                {" "}
+                {link.name}{" "}
+              </Link>
+            );
+          })}
         </div>
       </div>
-      <Image alt="profilpic" src={profilpic} />
+      {loggedIn ? (
+        <Image alt="profilpic" src={logoProfil} />
+      ) : (
+        <div className="flex items-center gap-3 ">
+          <button className="rounded-[4px] text-[#3A4F39] border-[#3A4F39] w-32 text-lg p-3 border "> Log In </button>
+          <button className="rounded-[4px] bg-[#3A4F39] text-white w-32 text-lg p-3 border "> Sign In </button>
+        </div>
+      )}
     </div>
   );
 };
+
+
+// generaleStyles={{
+//   textColor: "....",
+//   textSize: "...",0
+//   borderColor: "....",
+// }}
+// mobileSettings={{
+//   menuType: "hamburger", // Options: "hamburger", "sidebar", "bottom-bar", etc,
 
 export default Navbar;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,32 @@
-import Sidenavbar3 from "@/components/template3/SideNabBar"
-import Navbar from "@/components/template1/Navbar"
-const Home = () => {
-  return (
-    <div  className="w-full h-screen bg-black">
-   <Navbar/>
-    </div>
-  )
-}
+import Sidenavbar3 from "@/components/template3/SideNabBar";
+import Navbar from "@/components/template1/Navbar";
+import logo from "public/Images/template1/Navbar/Logo.svg";
+import profilpic from "public/Images/template1/Navbar/profilpic.svg";
 
-export default Home
+const Home = () => {
+  const links = [
+    {
+      name: "Home",
+      route: "/"
+    },
+    {
+      name: "Landing",
+      route: "/landing",
+    },
+    {
+      name: "About",
+      route: "/About",
+    },
+    {
+      name: "Contact",
+      route: "/",
+    },
+  ];
+  return (
+    <div className="w-full h-[150vh] bg-black">
+      <Navbar links={links} loggedIn logo={logo} textColor="#3A4F39" menuType="bottom-bar" textSize="18px" logoProfil={profilpic} layout="Fixed" />
+    </div>
+  );
+};
+
+export default Home;

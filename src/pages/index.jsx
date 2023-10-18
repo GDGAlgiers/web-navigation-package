5725fab3de4caff4c7e5ad97ae31e6a3747f3211
Ryaaad <devil.fine116@gmail.com>
2023-10-18 14:59:02 +0100

    Navbar1 custimised & responsive

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
-import Sidenavbar3 from "@/components/template3/SideNabBar";
 import Navbar from "@/components/template1/Navbar";
-import logo from "public/Images/template1/Navbar/Logo.svg";
+import logo from "public/Images/template1/Navbar/logo-botw.png";
 import profilpic from "public/Images/template1/Navbar/profilpic.svg";
 
 const Home = () => {
@@ -22,9 +21,10 @@ const Home = () => {
       route: "/",
     },
   ];
+
   return (
     <div className="w-full h-[150vh] bg-black">
-      <Navbar links={links} loggedIn logo={logo} textColor="#3A4F39" menuType="bottom-bar" textSize="18px" logoProfil={profilpic} layout="Fixed" />
+      <Navbar links={links} loginRoute="/login" menuType="bottom-bar" ButtonColor="red" SignInRoute="/Signin" logo={logo} textColor="#3A4F39" textSize="18px" logoProfil={profilpic} layout="Fixed" />
     </div>
   );
 };

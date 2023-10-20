033b0aaa1b139fe9407df42eddae1ecb67beb64c
Ryaaad <devil.fine116@gmail.com>
2023-10-20 23:37:47 +0100

    custimise sidenav3 responsive

--- src/components/template3/SideNabBar.jsx
+++ src/components/template3/SideNabBar.jsx
@@ -1,68 +1,115 @@
 import Image from "next/image";
 import Link from "next/link";
-import msg from "public/Images/template3/chat.svg"
-import setting from "public/Images/template3/setting.svg"
-import notif from "public/Images/template3/bell.svg"
-import logout from "public/Images/template3/sign-out.svg"
-import { useRouter } from 'next/router';
-
-
-
-
-
-// generaleStyles={{
-//   textColor: "....",
-//   textSize: "...",
-//   borderColor: "....",
-//   ....
-// }}
-
+import msg from "public/Images/template3/chat.svg";
+import setting from "public/Images/template3/setting.svg";
+import notif from "public/Images/template3/bell.svg";
+import logout from "public/Images/template3/sign-out.svg";
+import { useRouter } from "next/router";
 
 const Sidenavbar3 = (props) => {
-  const {logo , Name  , links , User , Userlinks }=props
+  const { logo, Name, links, User, textSize, Userlinks,ActivetextColor, ActiveBg, textColor } = props;
   const router = useRouter();
-const currentRoute = router.pathname;
+  const currentRoute = router.pathname;
 
   return (
-    <div className="w-[20vw] fixed top-0 scroll flex flex-col gap-1 items-center bg-white h-screen max-h-screen overflow-y-scroll p-5 ">
-      <div className="flex p-2 items-center gap-2">
-        <Image alt="logo" src={logo} />
-        <h1 className="text-[#324054] font-semibold text-2xl "> {Name} </h1>
-      </div>
-      <div className="flex w-full font-medium flex-col gap-3 mt-5 text-[#324054] ">
-        {links.map((link,index)=>{
-          return  <Link key={index} href={link.route}>
-          <div className={`p-2 flex items-center gap-4 ${link.route==currentRoute && "bg-[#EFF6FF]" }`}>
-            {link.icon}
-            <h1 className={`${link.route==currentRoute && "text-[#2D68FE]" }`}> {link.name} </h1>
+    <>
+      <div className="w-64 fixed top-0 scroll flex-col gap-1 items-center bg-white h-screen max-h-screen overflow-y-scroll p-5 hidden lg:flex ">
+        <div className="flex p-2 items-center gap-2">
+          <Image alt="logo" src={logo} />
+          <h1 className="text-[#324054] font-semibold text-2xl "> {Name} </h1>
+        </div>
+        <div
+          style={{ fontSize: `${textSize}`, color: `${textColor}` }}
+          className="flex w-full font-medium flex-col gap-3 mt-5 "
+        >
+          {links.map((link, index) => {
+            return (
+              <Link key={index} href={link.route}>
+                <div
+                style={{background:`${link.route == currentRoute && ActiveBg}`}}
+                  className={`p-2 flex items-center gap-4`}
+                >
+                  {link.icon}
+                  <h1
+                  style={{color:`${link.route == currentRoute && ActivetextColor}`}}
+                  >
+                    {" "}
+                    {link.name}{" "}
+                  </h1>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+        <div
+          style={{ fontSize: `${textSize}`, color: `${textColor}` }}
+          className="flex w-full font-medium flex-col gap-3 mt-8 "
+        >
+          {Userlinks.map((Userlink, index) => {
+            return (
+              <Link href={Userlink.route}>
+                <div className="flex items-center justify-between">
+                  <div className="p-2 flex items-center gap-4">
+                    {Userlink.icon}
+                    <h1 className=" "> {Userlink.name} </h1>
+                  </div>
+                  {Userlink.notif && (
+                    <div className="bg-[red] text-white w-4 h-4 flex items-center justify-center text-xs rounded-full">
+                      {Userlink.notif}
+                    </div>
+                  )}
+                </div>
+              </Link>
+            );
+          })}
+          <div className="flex items-center gap-2 p-3">
+            <Image alt="profil pic" src={User.pic} />
+            <div className=" text-xs ">
+              <h1 className="text-[#324054]">{User.name}</h1>
+              <p className="text-[#71839B] ">{User.email}</p>
+            </div>
           </div>
-        </Link>
-        })}
+        </div>
+      </div>
 
-      </div>
-   <div className="flex w-full font-medium flex-col gap-3 mt-8 text-[#324054] ">
-     {Userlinks.map((Userlink,index)=>{
-      return     <Link href={Userlink.route}>
-      <div className="flex items-center justify-between">
-        <div className="p-2 flex items-center gap-4">
-          {Userlink.icon}
-          <h1 className=" "> {Userlink.name} </h1>
+      <div className="w-32 fixed top-0 scroll flex-col gap-5 items-center bg-white h-screen max-h-screen p-5 px-2 flex lg:hidden ">
+        <div className="flex justify-center items-center">
+          <Image alt="logo" src={logo} />
         </div>
-      { Userlink.notif && <div className="bg-[red] text-white w-4 h-4 flex items-center justify-center text-xs rounded-full">
-          {Userlink.notif}
-        </div>}
-      </div>
-    </Link>
-     })}
-        <div className="flex items-center gap-2 p-3">
-        <Image alt="profil pic"  src={User.pic} />
-        <div className=" text-xs ">
-            <h1 className="text-[#324054]">{User.name}</h1>
-            <p className="text-[#71839B] ">{User.email}</p>
+        <div
+          style={{ fontSize: `${textSize}`, color: `${textColor}` }}
+          className="flex w-full font-medium flex-col gap-3 mt-5 "
+        >
+          {links.map((link, index) => {
+            return (
+              <Link key={index} href={link.route}>
+                <div className={`flex justify-center p-1 `}>{link.icon}</div>
+              </Link>
+            );
+          })}
+        </div>
+        <div
+          style={{ fontSize: `${textSize}`, color: `${textColor}` }}
+          className="flex w-full font-medium flex-col gap-3 mt-8 "
+        >
+          {Userlinks.map((Userlink, index) => {
+            return (
+              <Link key={index} href={Userlink.route}>
+                <div className="relative flex justify-center p-1 ">
+                  {Userlink.icon}
+                  {Userlink.notif && (
+                    <div className="bg-[red] absolute text-white w-1 h-1 top-0 right-[30%] rounded-full" />
+                  )}
+                </div>
+              </Link>
+            );
+          })}
+          <div className="flex justify-center p-3">
+            <Image alt="profil pic" src={User.pic} />
+          </div>
         </div>
       </div>
-  </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,13 @@ import Box from "public/Images/template3/box.svg";
 import analysis from "public/Images/template3/analysis.svg";
 import Image from "next/image";
 import Link from "next/link";
-import logout from "public/Images/template3/sign-out.svg"
-import msg from "public/Images/template3/chat.svg"
-import money from "public/Images/template3/money-bill.svg"
-import setting from "public/Images/template3/setting.svg"
-import notif from "public/Images/template3/bell.svg"
-import history from "public/Images/template3/clock-five.svg"
-import profil from "public/Images/template3/profilpic.svg"
+import logout from "public/Images/template3/sign-out.svg";
+import msg from "public/Images/template3/chat.svg";
+import money from "public/Images/template3/money-bill.svg";
+import setting from "public/Images/template3/setting.svg";
+import notif from "public/Images/template3/bell.svg";
+import history from "public/Images/template3/clock-five.svg";
+import profil from "public/Images/template3/profilpic.svg";
 import Sidenavbar3 from "@/components/template3/SideNabBar";
 import logo from "public/Images/template3/Logo.svg";
 
@@ -19,70 +19,76 @@ const Home = () => {
     {
       name: "Home",
       route: "/",
-      icon:  <Image alt="home" src={home} />
+      icon: <Image alt="home" src={home} />,
     },
     {
       name: "Dashboard",
       route: "/Dashboard",
-      icon:  <Image alt="home" src={Dashboard} />
-
+      icon: <Image alt="home" src={Dashboard} />,
     },
     {
       name: "Products",
       route: "/Products",
-      icon:  <Image alt="box" src={Box} />
-
+      icon: <Image alt="box" src={Box} />,
     },
     {
       name: "Analytics",
       route: "/Analytics",
-      icon: <Image alt="analysis" src={analysis} />
+      icon: <Image alt="analysis" src={analysis} />,
     },
     {
       name: "History",
       route: "/History",
-      icon: <Image alt="history" src={history} />
+      icon: <Image alt="history" src={history} />,
     },
     {
       name: "Sales",
       route: "/Sales",
-      icon: <Image alt="Sales" src={money} />
-    }
+      icon: <Image alt="Sales" src={money} />,
+    },
   ];
   const Userlinks = [
     {
       name: "Message",
       route: "/Message",
-      icon:  <Image alt="msg" src={msg} />,
-      notif:5
+      icon: <Image alt="msg" src={msg} />,
+      notif: 5,
     },
     {
       name: "Notifications",
       route: "/notif",
-      icon:  <Image alt="notif" src={notif} />,
-      notif:2
-
+      icon: <Image alt="notif" src={notif} />,
+      notif: 2,
     },
     {
       name: "Settings",
       route: "/setting",
-      icon:  <Image alt="setting" src={setting} />
-
+      icon: <Image alt="setting" src={setting} />,
     },
     {
       name: "Log out",
       route: "/logout",
-      icon: <Image alt="logout" src={logout} />
-    }
+      icon: <Image alt="logout" src={logout} />,
+    },
   ];
-  const user={
-    name:"Michael Smith",
-    email:"michaelsmith12@gmail.com",
-    pic:profil
-  }
+  const user = {
+    name: "Michael Smith",
+    email: "michaelsmith12@gmail.com",
+    pic: profil,
+  };
   return (
     <div className="w-full h-[150vh] bg-black">
-      <Sidenavbar3 logo={logo} Name="Social Blade" Userlinks={Userlinks} links={links} User={user} />
+      <Sidenavbar3
+        logo={logo}
+        Name="Social Blade"
+        textSize="16px"
+        textColor="red"
+        ActivetextColor="green"
+        ActiveBg="red"
+        Userlinks={Userlinks}
+        links={links}
+        User={user}
+      />
     </div>
   );
 };

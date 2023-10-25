1f5f123386e8a7a46686f58cb48c9a5ea5374988
imadeddineF <imadeddinefillalipro@gmail.com>
2023-10-25 10:54:38 +0100

    navbar three package is done

--- src/components/navbar3/navbarThree.jsx
+++ src/components/navbar3/navbarThree.jsx
@@ -22,13 +22,20 @@ export default function NavbarThree({
 
   const handleClick = () => {
     setIsOpen(!isOpen);
+    setVisibleTitles(false);
   };
 
   const handleTitleClick = (title) => {
-    setVisibleTitles((prevVisibleTitles) => ({
-      ...prevVisibleTitles,
-      [title]: !prevVisibleTitles[title],
-    }));
+    setVisibleTitles((prevVisibleTitles) => {
+      const updatedVisibleTitles = { [title]: !prevVisibleTitles[title] };
+      // Close all other popups
+      Object.keys(prevVisibleTitles).forEach((prevTitle) => {
+        if (prevTitle !== title) {
+          updatedVisibleTitles[prevTitle] = false;
+        }
+      });
+      return updatedVisibleTitles;
+    });
   };
 
   return (
@@ -40,7 +47,28 @@ export default function NavbarThree({
       className="flex items-center justify-between px-[30px]"
     >
       <div className="flex items-center justify-center gap-[40px]">
-        <Link href="/" className="">
+        {/* Mobile */}
+        <button
+          className="z-50 flex flex-col items-center justify-center my-2 cursor-pointer lg:hidden"
+          onClick={handleClick}
+        >
+          <span
+            className={`bg-gray-700 transition-all duration-300 h-[2px] w-6 rounded-sm  ${
+              isOpen ? "rotate-45 translate-y-[2.5px]" : "-translate-y-1"
+            }`}
+          ></span>
+          <span
+            className={`bg-gray-700 transition-all duration-300 h-[2px] w-6 rounded-sm ${
+              isOpen ? "opacity-0" : "opacity-100"
+            }`}
+          ></span>
+          <span
+            className={`bg-gray-700 transition-all duration-300 h-[2px] w-6 rounded-sm ${
+              isOpen ? "-rotate-45 -translate-y-[2.5px]" : "translate-y-1"
+            }`}
+          ></span>
+        </button>
+        <Link href="/" className="z-50 ">
           <Image src={logo} width={logoWidth} height={logoHeight} />
         </Link>
         <ul className="hidden lg:flex items-center justify-center gap-[20px]">
@@ -61,9 +89,9 @@ export default function NavbarThree({
                     animate={{ opacity: 1 }}
                     exit={{ y: -5, opacity: 0 }}
                     style={{
-                      top: "52px",
+                      top: `calc(${navHeight}/2 + 15px)`,
                     }}
-                    className="absolute left-0 flex flex-col border-2 w-[120px]"
+                    className="shadow-gray-500/40 overflow-hidden shadow-lg rounded-[8px] absolute left-0 flex flex-col w-[120px]"
                     onClick={() => handleTitleClick(title)}
                   >
                     {subTitles.map((subTitle, index) => (
@@ -85,53 +113,33 @@ export default function NavbarThree({
           ))}
         </ul>
       </div>
-      <div className="hidden lg:flex items-center justify-center gap-[20px]">
+      <div className="flex items-center justify-center gap-[20px]">
         <Link
           href={linkBtnOne}
-          className="border-[2px] border-blue-400 bg-green-300 hover:bg-green-600 transition-all duration-300 px-[20px] py-[10px] rounded-md "
+          className="hidden whitespace-nowrap lg:block border-[1px] border-[#3A4F39] bg-white hover:bg-[#3A4F39] hover:text-white transition-all duration-300 px-[20px] py-[10px] rounded-[4px]"
         >
           {btnOne}
         </Link>
         <Link
           href={linkBtnTwo}
-          className="border-[2px] border-blue-400 bg-green-300 hover:bg-green-600 transition-all duration-300 px-[20px] py-[10px] rounded-md "
+          className="z-50 whitespace-nowrap border-[1px] border-[#3A4F39] text-white bg-[#3A4F39] hover:bg-white hover:text-[#3A4F39] transition-all duration-300 px-[20px] py-[10px] rounded-[4px]"
         >
           {btnTwo}
         </Link>
       </div>
 
-      {/* Mobile */}
-      <button
-        className="z-50 flex flex-col items-center justify-center my-2 cursor-pointer lg:hidden"
-        onClick={handleClick}
-      >
-        <span
-          className={`bg-red-700 transition-all duration-300 h-[2px] w-6 rounded-sm  ${
-            isOpen ? "rotate-45 translate-y-[2.5px]" : "-translate-y-1"
-          }`}
-        ></span>
-        <span
-          className={`bg-red-700 transition-all duration-300 h-[2px] w-6 rounded-sm ${
-            isOpen ? "opacity-0" : "opacity-100"
-          }`}
-        ></span>
-        <span
-          className={`bg-red-700 transition-all duration-300 h-[2px] w-6 rounded-sm ${
-            isOpen ? "-rotate-45 -translate-y-[2.5px]" : "translate-y-1"
-          }`}
-        ></span>
-      </button>
-
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            initial={{ x: 100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: 100, opacity: 0 }}
+            initial={{ y: -200, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -200, opacity: 0 }}
             style={{
               backgroundColor: mobileSideBarBg,
+              height: `calc(100vh - ${navHeight})`,
+              marginTop: navHeight,
             }}
-            className="flex pt-[100px] gap-[50px] flex-col w-full sm:w-[300px] h-[99.9%] absolute top-0 right-0"
+            className="flex border-t-[#3A4F39] border-t-[2px] pt-[15px] gap-[50px] flex-col w-full h-[100vh] absolute top-0 left-0"
           >
             <ul className="flex flex-col items-center w-full lg:hidden">
               {data.map(({ title, subTitles, subTitlesLinks }, index) => (
@@ -171,16 +179,12 @@ export default function NavbarThree({
               ))}
             </ul>
 
-            <div className="lg:hidden flex flex-col items-center justify-center gap-[10px]">
-              <Link
-                href={linkBtnOne}
-                className="border-[2px] border-blue-200 w-[100px] items-center flex justify-center bg-green-300 hover:bg-green-600 transition-all duration-300 py-[10px] rounded-[4px] "
-              >
-                {btnOne}
-              </Link>
+            <div className="flex flex-col lg:hidden">
+              <div className="h-[2px] self-center w-[95%] bg-[#3A4F39]" />
+
               <Link
                 href={linkBtnTwo}
-                className="border-[2px] border-blue-200 items-center flex justify-center w-[100px] bg-green-300 hover:bg-green-600 transition-all duration-300 py-[10px] rounded-[4px]"
+                className="border-[1.5px] ml-[70px] border-[#3A4F39] items-center mt-[20px] flex justify-center w-[100px] text-[#3A4F39] bg-white hover:text-white hover:bg-[#3A4F39] transition-all duration-300 py-[10px] rounded-[4px]"
               >
                 {btnTwo}
               </Link>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,17 @@
 import SideBarWithSearch from "@/components/SidebarWithSearch/SideBarWithSearch";
 import logo from "public/Images/SideBarWithSearch/logo.svg";
 import profilePicture from "public/Images/SideBarWithSearch/profile_picture.svg";
-import objectsSets from "public/Images/SideBarWithSearch/object_sets.svg"
-import singleObjects from "public/Images/SideBarWithSearch/single_objects.svg"
-import weeklyTop from "public/Images/SideBarWithSearch/weekly_top.svg"
-import settingsPic from "public/Images/SideBarWithSearch/settings.svg"
-import faq from "public/Images/SideBarWithSearch/faq.svg"
-import heart from "public/Images/SideBarWithSearch/heart.svg"
-import gallery from "public/Images/SideBarWithSearch/gallery.svg"
-import cart from "public/Images/SideBarWithSearch/cart.svg"
+import objectsSets from "public/Images/SideBarWithSearch/object_sets.svg";
+import singleObjects from "public/Images/SideBarWithSearch/single_objects.svg";
+import weeklyTop from "public/Images/SideBarWithSearch/weekly_top.svg";
+import settingsPic from "public/Images/SideBarWithSearch/settings.svg";
+import faq from "public/Images/SideBarWithSearch/faq.svg";
+import heart from "public/Images/SideBarWithSearch/heart.svg";
+import gallery from "public/Images/SideBarWithSearch/gallery.svg";
+import cart from "public/Images/SideBarWithSearch/cart.svg";
 import Image from "next/image";
+import nvb3Logo from "public/Images/navbar3/Logo.svg";
+import NavbarThree from "@/components/navbar3/navbarThree";
 
 const Home = () => {
   const profileInformation = {
@@ -19,75 +21,117 @@ const Home = () => {
   };
 
   const settings = [
-      {
-          image:<Image src={settingsPic} alt="settings" />,
-          text:"settings",
-          route:"/settings",
-      },
-      {
-          image:<Image src={faq} alt="FAQ"/>,
-          text:"FAQ",
-          route:"/faq",
-      },
-  ]
+    {
+      image: <Image src={settingsPic} alt="settings" />,
+      text: "settings",
+      route: "/settings",
+    },
+    {
+      image: <Image src={faq} alt="FAQ" />,
+      text: "FAQ",
+      route: "/faq",
+    },
+  ];
 
-  const options =[
-      {
-          image:<Image src={heart} alt="fav"/>,
-          text:"Favourites",
-          route:"/favourites",
-      },
-      {
-          image: <Image src={gallery} alt="gallery"/>,
-          text:"My gallery",
-          route:"/gallery",
-      },
-      {
-          image:<Image src={cart} alt="cart" />,
-          text:"My Cart",
-          route:"/cart",
-      }
-  ]
+  const options = [
+    {
+      image: <Image src={heart} alt="fav" />,
+      text: "Favourites",
+      route: "/favourites",
+    },
+    {
+      image: <Image src={gallery} alt="gallery" />,
+      text: "My gallery",
+      route: "/gallery",
+    },
+    {
+      image: <Image src={cart} alt="cart" />,
+      text: "My Cart",
+      route: "/cart",
+    },
+  ];
 
   const instances = [
-        {
-            image:<Image src={weeklyTop} alt="weekly top" width={15}/>,
-            text:"Weekly top",
-            route:"/weekly-top"
-        },
-        {
-            image: <Image src={singleObjects} alt="Single objects" width={15}/>,
-            text:"Single objects",
-            route:"/single-objects"
-        },
-        {
-            image : <Image src={objectsSets} alt="Objects sets" width={15} />,
-            text:"Objects sets",
-            route:"/objects-sets"
-        },
+    {
+      image: <Image src={weeklyTop} alt="weekly top" width={15} />,
+      text: "Weekly top",
+      route: "/weekly-top",
+    },
+    {
+      image: <Image src={singleObjects} alt="Single objects" width={15} />,
+      text: "Single objects",
+      route: "/single-objects",
+    },
+    {
+      image: <Image src={objectsSets} alt="Objects sets" width={15} />,
+      text: "Objects sets",
+      route: "/objects-sets",
+    },
   ];
 
   const generalStyles = {
-      textSize: "0.75rem",
-      logoTextSize:"1.25rem",
-      textColor:"#969998",
-      activeTextColor:"white",
-      bg:"#171918",
-      activeBg:"#27AE60",
-      seperatingLineColor:"#FFFFFF26",
-  }
+    textSize: "0.75rem",
+    logoTextSize: "1.25rem",
+    textColor: "#969998",
+    activeTextColor: "white",
+    bg: "#171918",
+    activeBg: "#27AE60",
+    seperatingLineColor: "#FFFFFF26",
+  };
 
   const searchBarCustomization = {
-      placeholder:"search instances",
-      options:{},
-      bg:"#3A3B3B",
-      textSize: "0.8rem",
-      textColor: "#7F8080",
-  }
+    placeholder: "search instances",
+    options: {},
+    bg: "#3A3B3B",
+    textSize: "0.8rem",
+    textColor: "#7F8080",
+  };
+
+  const randomDataLinks = [
+    {
+      title: "One",
+      subTitles: ["One", "Two", "Three"],
+      subTitlesLinks: ["/", "/", "/"],
+    },
+    {
+      title: "Two",
+      subTitles: ["Four", "Five", "Six"],
+      subTitlesLinks: ["/", "/", "/"],
+    },
+    {
+      title: "Three",
+      subTitles: ["Seven", "Eight", "Nine"],
+      subTitlesLinks: ["/", "/", "/"],
+    },
+  ];
 
   return (
-    <div className="w-full h-[150vh] bg-black">
-      <SideBarWithSearch logo={<Image src={logo} alt="logo" />} name="ModGallery" searchBarCustomization={searchBarCustomization}  profileInformation={profileInformation} instances={instances} settings={settings} options={options} layout="Fixed" loggedIn={true} generalStyles={generalStyles}/>
+    <div className="">
+      <NavbarThree
+        logo={nvb3Logo}
+        logoWidth="70px"
+        logoHeight=""
+        data={randomDataLinks}
+        btnOne="Sigh in"
+        linkBtnOne="/"
+        btnTwo="Sign up"
+        linkBtnTwo="/"
+        navBgColor="#dddddd"
+        navHeight="80px"
+        mobileSideBarBg="#fff"
+      />
+      {/* <SideBarWithSearch
+        logo={<Image src={logo} alt="logo" />}
+        name="ModGallery"
+        searchBarCustomization={searchBarCustomization}
+        profileInformation={profileInformation}
+        instances={instances}
+        settings={settings}
+        options={options}
+        layout="Fixed"
+        loggedIn={true}
+        generalStyles={generalStyles}
+      /> */}
     </div>
   );
 };

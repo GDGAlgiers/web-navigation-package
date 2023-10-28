f435a1419e5beb79e3c1c061015ca53c350bd438
salah eddine <slhokli2002@gmail.com>
2023-10-28 19:42:50 +0100

    added mobile search menu

--- src/components/template2/Navbar.jsx
+++ src/components/template2/Navbar.jsx
@@ -14,26 +14,15 @@ function Navbar(props) {
     mobileSettings,
   } = props;
 
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const updateScreenWidth = () => {
-    setScreenWidth(window.innerWidth);
-  };
-  useEffect(() => {
-    window.addEventListener("resize", updateScreenWidth);
-    return () => {
-      window.removeEventListener("resize", updateScreenWidth);
-    };
-  }, []);
+  const [searchOpen, setSearchOpen] = useState(false);
   return (
-    <div className="relative w-screen">
+    <div className="relative w-screen ">
       <div className="flex  justify-between items-center bg-white h-24 w-full 2xl:px-16 xl:px-8 px-4">
         <div className="flex items-center justify-start h-ful xl:gap-20 2xl:gap-24 gap-10">
+          <HamMenu className="lg:hidden h-6 w-auto cursor-pointer" />
           <Image src={logo} alt="logo" className=" h-20" />
 
-          <SearchBar
-            className="my-5 h-auto 2xl:w-[510px] w-[400px]"
-            hidden={screenWidth < 720}
-          />
+          <SearchBar className="hidden md:flex my-5 h-auto 2xl:w-[510px] w-[400px]" />
         </div>
 
         <div className="flex gap-8 items-center h-14">
@@ -46,41 +35,64 @@ function Navbar(props) {
               </li>
             ))}
           </ul>
-
+          <button
+            className="md:hidden"
+            onClick={() => setSearchOpen((prv) => !prv)}
+          >
+            <Image src={searchIcon} />
+          </button>
           <Link href="#">
             <Image src={avatar} />
           </Link>
-          <HamMenu className="lg:hidden h-6 w-auto cursor-pointer" />
         </div>
-        {/* mobile menu */}
-        {/* <div className="absolute flex flex-col items-center w-full h-auto bg-white top-full left-0">
-          <ul className="flex flex-col p-4">
-            <li className="py-2">
-              <SearchBar className="w-[300px] h-auto" />
-            </li>
-            <li className="py-2">
-              <Link href="#">
-                <Image src={settingsIcon} className="h-full" />
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="#">
-                <Image src={settingsIcon} className="h-full" />
-              </Link>
-            </li>
-          </ul>
-        </div> */}
       </div>
+      {/* mobile menu */}
+      <div className="absolute flex flex-col items-center w-full h-auto bg-white top-full left-0">
+        <ul className="flex flex-col p-4">
+          <li className="py-2">
+            <SearchBar className="w-[300px] h-auto" />
+          </li>
+          <li className="py-2">
+            <Link href="#">
+              <Image src={settingsIcon} className="h-full" />
+            </Link>
+          </li>
+          <li className="py-2">
+            <Link href="#">
+              <Image src={settingsIcon} className="h-full" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/* mobile search */}
+      {searchOpen && (
+        <div className="absolute py-5 flex justify-center gap-4 items-center w-full h-auto bg-white top-full left-0">
+          <div
+            className={`flex w-64 h-12 items-center border border-[#696969] rounded-[100px] justify-between p-4 bg-Light-grey-input text-[#696969]`}
+          >
+            <Image src={searchIcon} />
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-transparent outline-none pl-5 flex-grow"
+            />
+          </div>
+          <button
+            className="text-[#696969]"
+            onClick={() => setSearchOpen(false)}
+          >
+            annuler
+          </button>
+        </div>
+      )}
     </div>
   );
 }
 
-function SearchBar({ className, hidden }) {
+function SearchBar({ className }) {
   return (
     <div
-      className={`${className} ${
-        hidden ? "hidden" : "flex"
-      } items-center rounded justify-between p-4 bg-Light-grey-input`}
+      className={`${className} items-center rounded justify-between p-4 bg-Light-grey-input`}
     >
       <input
         type="text"
